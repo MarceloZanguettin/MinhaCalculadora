8306eb113bf2c76a9d25e06a28756c019200ee65
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2022-04-11 19:35:12 +0000

    Exemplo Calculadora

---
+++ Calculadora.java
@@ -0,0 +1,9 @@
+public class Calculadora {
+
+    public static void main(String[] args){
+        MinhaCalculadora soma1 = new MinhaCalculadora();
+        System.out.println(soma1.soma(38,12));
+        System.out.println(soma1.soma(47.3,52.7));
+    }
+    
+}
